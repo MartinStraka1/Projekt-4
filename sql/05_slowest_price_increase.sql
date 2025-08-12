5ba1b0d879a0498fcdbcac64029881a5c65e1c0b
MartinStraka1 <martin.b.straka@gmail.com>
2025-08-12 07:21:19 +0200

    Update 05_slowest_price_increase.sql

--- sql/05_slowest_price_increase.sql
+++ sql/05_slowest_price_increase.sql
@@ -1,28 +1,42 @@
 WITH cenovy_vyvoj AS (
-  -- 1) Vypočítáme pro každou kategorii potravin a rok
-  --    průměrnou cenu a cenu z předchozího roku
-  SELECT DISTINCT
-    potravina,                                           -- název potraviny
-    rok,                                                 -- rok
-    prumerna_cena,                                       -- průměrná cena v daném roce
+-- 1) Vypočítáme pro každou kategorii potravin a rok
+--    průměrnou cenu a cenu z předchozího roku
+SELECT
+    DISTINCT
+    potravina,
+    -- název potraviny
+    rok,
+    -- rok
+    prumerna_cena,
+    -- průměrná cena v daném roce
     LAG(prumerna_cena) OVER (PARTITION BY potravina
-                              ORDER BY rok)            -- cena minulý rok
+ORDER BY
+    rok)
+    -- cena minulý rok
       AS cena_minuly_rok
-  FROM data_academy_content.t_martin_straka_project_SQL_primary_final
+FROM
+    data_academy_content.t_martin_straka_project_SQL_primary_final
 )
-
 -- 2) Vybereme jen kategorie, kde cena **rostla** a spočítáme % změnu
 SELECT
-  potravina,                                            -- název potraviny
-  rok,                                                  -- rok, kdy došlo k růstu
-  ROUND(
-    (prumerna_cena - cena_minuly_rok)                   -- absolutní rozdíl
-    / NULLIF(cena_minuly_rok, 0) * 100                   -- přepočet na procenta
-  , 2) AS pct_zmena                                      -- meziroční % změna
-FROM cenovy_vyvoj
+    potravina,
+    -- název potraviny
+    rok,
+    -- rok, kdy došlo k růstu
+    ROUND(
+    (prumerna_cena - cena_minuly_rok)-- absolutní rozdíl
+    / NULLIF(cena_minuly_rok, 0) * 100 -- přepočet na procenta
+  , 2) AS pct_zmena
+    -- meziroční % změna
+FROM
+    cenovy_vyvoj
 WHERE
-  cena_minuly_rok IS NOT NULL                           -- vynecháme první rok
-  AND prumerna_cena > cena_minuly_rok                    -- pouze kategorie se zdražením
+    cena_minuly_rok IS NOT NULL
+    -- vynecháme první rok
+    AND prumerna_cena > cena_minuly_rok
+    -- pouze kategorie se zdražením
 ORDER BY
-  pct_zmena ASC                                          -- od nejpomalejšího růstu
-LIMIT 1;                                                 -- jen ta jedna nejpomalejší
+    pct_zmena ASC
+    -- od nejpomalejšího růstu
+LIMIT 1;
+-- jen ta jedna nejpomalejší
